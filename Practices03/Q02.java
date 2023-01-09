28ba504d9c6c5c842d0fdaf2156b10d2942d874f
Ikbal Aslan <ikball.aslann@gmail.com>
2023-01-09 21:31:31 +0300

    Upd-2

---
+++ Practices03/Q02.java
@@ -0,0 +1,34 @@
+package practices_with_group.Practice04;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import utilities.TestBase;
+
+import static org.junit.Assert.assertEquals;
+
+public class Q02 extends TestBase {
+
+        /*
+      Given
+          Go to https://testpages.herokuapp.com/styled/challenges/growing-clickable.html
+      When
+          Click on "click me" button
+      Then
+          Verify that "Event Triggered"
+       */
+        @Test
+        public void test()  {
+//        Go to https://testpages.herokuapp.com/styled/challenges/growing-clickable.html
+            driver.get("https://testpages.herokuapp.com/styled/challenges/growing-clickable.html");
+
+//        Click on "click me" button
+            wait_for(4);
+            driver.findElement(By.id("growbutton")).click();
+
+//        Verify that "Event Triggered"
+            String growButtonStatus = driver.findElement(By.id("growbuttonstatus")).getText();
+            assertEquals("Event Triggered",growButtonStatus);
+
+        }
+}
