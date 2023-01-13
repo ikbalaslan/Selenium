05aec82cdfe0b2cb0a4ea96ffa971fe7159b1b86
Ikbal Aslan <ikball.aslann@gmail.com>
2023-01-13 22:14:18 +0300

    testNG

--- src/test/java/practices_with_group/Practice06/Q02.java
+++ src/test/java/practices_with_group/Practice06/Q02.java
@@ -5,7 +5,10 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import utilities.TestBase;
 
+import java.text.DecimalFormat;
 import java.util.List;
+
+import static org.junit.Assert.assertEquals;
 
 public class Q02 extends TestBase {
     //We will make a roof of 5% of the height of the earliest built tower.
@@ -26,7 +29,7 @@ public class Q02 extends TestBase {
      */
 
     @Test
-    public void test(){
+    public void test() {
 //        Go to https://www.techlistic.com/p/demo-selenium-practice.html
         driver.get("https://www.techlistic.com/p/demo-selenium-practice.html");
 
@@ -36,9 +39,9 @@ public class Q02 extends TestBase {
 
 //        Calculate min year
         int minYear = Integer.parseInt(builtYears.get(0).getText());
-        for (WebElement w : builtYears){
+        for (WebElement w : builtYears) {
 
-            if(Integer.parseInt(w.getText())<minYear){
+            if (Integer.parseInt(w.getText()) < minYear) {
                 minYear = Integer.parseInt(w.getText());
             }
 
@@ -47,25 +50,27 @@ public class Q02 extends TestBase {
 
 //        Find index of min year
 
-        int idxOfMinYear=0;
-        for(int i=0; i< builtYears.size(); i++){
+        int idxOfMinYear = 0;
+        for (int i = 0; i < builtYears.size(); i++) {
 
-            if(Integer.parseInt(builtYears.get(i).getText())==minYear){
-                idxOfMinYear=i;
+            if (Integer.parseInt(builtYears.get(i).getText()) == minYear) {
+                idxOfMinYear = i;
             }
         }
         System.out.println("idxOfMinYear = " + idxOfMinYear);
 
-
 //        Put all heights into a list
+        List<WebElement> heights = driver.findElements(By.xpath("(//tbody)[2]//td[3]"));
 
 //        Calculate the height to build
+        double heightToBuild = Integer.parseInt(heights.get(idxOfMinYear).getText().replaceAll("\\D", "")) * 0.05;
 
 //        Assert that build height is 25.45 meters
-
-
-
-
+        System.out.println("heightToBuild = " + heightToBuild);
+        DecimalFormat decimalFormat = new DecimalFormat("0.00");
+        String formattedHeight = decimalFormat.format(heightToBuild);
+        System.out.println("You will build: " + formattedHeight + " meters");
+        assertEquals("25.45", formattedHeight);
 
     }
 }
